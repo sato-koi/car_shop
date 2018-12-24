688399b82b10665712e1bc8e750a06e9c524cf62
sato-koi <sato.koichi.0404@gmail.com>
2018-12-25 00:00:20 +0900

    登録機能作成

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,20 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.save
+    redirect_to @car, notice: "車両を登録しました。"
+  end
+
+  private
+
+  def car_params
+    params.reqire(:car).permit(:title, :price, :release_date, :description)
+  end
 end
