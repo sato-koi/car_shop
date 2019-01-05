b87ce68c3bbe312b53104ca89ad73092220c0346
sato-koi <sato.koichi.0404@gmail.com>
2019-01-06 01:02:29 +0900

    コールバック作成

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ApplicationRecord
+  has_one_attached :image
   validates :name, presence: true, length: { maximum: 70 }
   validates :price, presence: true,
   numericality: { 
@@ -7,4 +8,7 @@ class Car < ApplicationRecord
   }
   validates :release_date, presence: true
   validates :description, presence: true, length: { maximum: 2000 }
+  before_save do
+    self.image = new_image if new_image
+  end
 end
