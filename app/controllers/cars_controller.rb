e1a8f4ec199e5800b0cc7447ead8d36c17f101f7
sato-koi <sato.koichi.0404@gmail.com>
2019-01-24 23:04:49 +0900

    ページネーション作成

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,8 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @cars = Car.all
+    @cars = Car.page(params[:page]).per(4)
   end
   
   def new
